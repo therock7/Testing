59adfbdbb930373f4035d523ed82f9a987762343
BanyA7X <50118567+BanyVengeanceA7X@users.noreply.github.com>
2020-05-16 15:15:08 +0700

    ini adalah test upload yg kedua

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package test;
 
-/**
- *
- * @author BANY A7X
- */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello");
+        
+        System.out.println("push yg kedua");
     }
     
 }
